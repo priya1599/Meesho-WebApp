08925ac94582aad1a4fdcf3de7eccb8b5c19d264
Priya Deshmukh <priyadeshmukh15.pd@gmail.com>
2024-06-02 10:12:44 +0530

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 export async function productsData(){
     const products = await axios.get(
-        "https://fakestoreapi.com/products"
+        "https://fakestoreapiserver.vercel.app/amazonproducts"
         );
         
     return products
